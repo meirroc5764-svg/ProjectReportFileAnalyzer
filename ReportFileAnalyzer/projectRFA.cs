ff2b6c18eaebe104bcea4337b3ded0635b79eedb
Meir <meirroc5764@gmail.com>
2026-06-25 10:43:39 +0300

    adds three static method with fix problen Score . to ,

--- ReportFileAnalyzer/projectRFA.cs
+++ ReportFileAnalyzer/projectRFA.cs
@@ -42,16 +42,18 @@ namespace projectRFA
                 string[] allData = File.ReadAllLines(path);
                 if (allData.Length == 0)
                     return null;
+                int count = allData.Length;
+                Console.WriteLine($"File loaded:{count} lines found");
                 return allData;
             }
             else
                 return null;
 
         }
-        static string ProcessReports(string[] allData, string[] unit, ClassReportsType[] reportType, int[] priority, double[] score, ClassStatus[] status)
+        static void ProcessReports(string[] allData, string[] unit, ClassReportsType[] reportType, int[] priority, double[] score, ClassStatus[] status)
         {
 
-            for (int index = 0; index == allData.Length; index++)
+            for (int index = 0; index < allData.Length; index++)
             {
                 ClassReportsType ReportType;
                 int Priority;
@@ -59,41 +61,85 @@ namespace projectRFA
                 ClassStatus Status;
 
 
-                if (line == null)
+                if (allData[index] == null)
                     continue;
-                string cleanLine = string.Join(",", allData[index].Split(" ", StringSplitOptions.RemoveEmptyEntries));
+                string[] cleanLine = allData[index].Split(",", StringSplitOptions.RemoveEmptyEntries);
 
-                if (!ClassReportsType.Tryparse(cleanLine[1], true, out ReportType))
+                if (cleanLine.Length != 5)
+                {
+                    Console.WriteLine("Invalid len");
                     continue;
+                }
+                    
 
-                else if (!ClassStatus.TryParse(cleanLine[4], true, out Status))
+                if (!Enum.TryParse<ClassReportsType>(cleanLine[1].Trim(), true, out ReportType))
+                {
+                    Console.WriteLine($"Invalid Report Type: {cleanLine[1].Trim()}");
                     continue;
+                }
 
-                else if (!int.TryParse(cleanLine[2], true, out Priority))
+                else if (!Enum.TryParse<ClassStatus>(cleanLine[4].Trim(), true, out Status))
+                {
+                    Console.WriteLine($"Invalid Status: {cleanLine[4].Trim()}");
                     continue;
+                }
 
-                else if (0 > priority || priority > 6)
+                else if (!int.TryParse(cleanLine[2], out Priority))
+                {
+                    Console.WriteLine($"Invalid Priority: {cleanLine[2]}");
                     continue;
+                }
 
-                else if (!double.TryParse(cleanLine[3], true, out Score))
+                else if (0 > Priority || Priority > 6)
+                {
+                    Console.WriteLine($"Invalid Priority: {cleanLine[2].Trim()}");
                     continue;
+                }
+
+                else if (!double.TryParse(cleanLine[3].Trim(), System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out Score))
+                {
+                    Console.WriteLine($"Invalid Score: {cleanLine[3]}");
+                    continue;
+                }
 
                 else if (Score > 100.0 || Score < 0.0)
+                {
+                    Console.WriteLine($"Invalid Score: {cleanLine[3]}!");
                     continue;
+                }
 
                 unit[index] = cleanLine[0];
                 reportType[index] = ReportType;
                 priority[index] = Priority;
                 score[index] = Score;
                 status[index] = Status;
-
-
+                Console.WriteLine("Valid record processed.");
+                Console.WriteLine($"unit: {cleanLine[0]}, reportType: {ReportType}, priority: {Priority}, score: {Score}, status: {Status}");
 
             }
-            return $"File loaded:{uint.Length} lines found";
-                
         }
+        static double CalculateAverage(double[] Score)
+        {
+                double average = 0;
+                foreach(double score in Score)
+                {
+                    average += score;
+                }
+                return average / Score.Length;
             
-        
+
+        }
+        static double FindMaxScore(double[] Score)
+        {
+            double maxScore = 0;
+            foreach (double score in Score)
+            {
+                if (score > maxScore)
+                    maxScore = score;
+            }
+                
+            return maxScore;
+        }
+        static 
     }
 }
